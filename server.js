55f400cd306d3d27ba043569ffda683ebc8e5da1
acottam <adam@gofishcreative.com>
2026-03-13 20:01:15 -0700

    Step 2: Add Swagger API Documentation to your project

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 app.use('/', require('./routes'));
 
 database.initDb((err) => {
